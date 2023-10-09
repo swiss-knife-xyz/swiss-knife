0033b16174bd00cbd12ebda82bd86977196a3c5d
apoorvlathey <latheyapoorv@gmail.com>
2023-10-09 15:22:27 +0530

    fix: startHexWith0x for undefined

--- utils/index.ts
+++ utils/index.ts
@@ -46,8 +46,8 @@ export const startHexWith0x = (hexValue?: string): Hex => {
   return hexValue
     ? hexValue.startsWith("0x")
       ? hexValue === "0x"
-        ? "0x00"
+        ? "0x"
         : (hexValue as Hex)
       : `0x${hexValue}`
-    : "0x00";
+    : "0x";
 };
