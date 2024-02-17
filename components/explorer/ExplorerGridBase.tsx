8994bcdcff536f188fd284d7ac0800f7bd913898
apoorvlathey <latheyapoorv@gmail.com>
2024-02-17 20:36:48 +0530

    feat: add filter by chain

--- components/explorer/ExplorerGridBase.tsx
+++ components/explorer/ExplorerGridBase.tsx
@@ -3,20 +3,46 @@ import {
   Box,
   Button,
   Center,
+  HStack,
   Input,
   InputGroup,
   InputRightElement,
   SimpleGrid,
+  Text,
+  Image,
+  GridItem,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverBody,
 } from "@chakra-ui/react";
 import { CloseIcon } from "@chakra-ui/icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
 import { ExplorerGridItem } from "./ExplorerGridItem";
 import { ExplorersData, ExplorerType } from "@/types";
+import { c, chainIdToChain, chainIdToImage } from "@/data/common";
 
 interface Params {
   explorersData: ExplorersData;
   explorerType: ExplorerType;
   addressOrTx: string;
 }
+
+const chainIdOptions = [
+  c.mainnet.id,
+  c.arbitrum.id,
+  c.avalanche.id,
+  c.base.id,
+  c.bsc.id,
+  c.gnosis.id,
+  c.optimism.id,
+  c.polygon.id,
+  c.zkSync.id,
+  c.zora.id,
+];
 
 export const ExplorerGridBase = ({
   explorersData,
@@ -27,20 +53,31 @@ export const ExplorerGridBase = ({
   const [filteredExplorerNames, setFilteredExplorerNames] = useState<string[]>(
     []
   );
+  const [chainIdsSelected, setChainIdsSelected] = useState<number[]>([]);
 
   useEffect(() => {
     setFilteredExplorerNames(
-      Object.keys(explorersData).filter((explorerName) => {
-        if (!searchExplorer) return true;
+      Object.keys(explorersData)
+        .filter((explorerName) => {
+          if (!searchExplorer) return true;
 
-        return (
-          explorerName
-            .toLowerCase()
-            .indexOf(searchExplorer.toLocaleLowerCase()) !== -1
-        );
-      })
+          return (
+            explorerName
+              .toLowerCase()
+              .indexOf(searchExplorer.toLocaleLowerCase()) !== -1
+          );
+        })
+        .filter((explorerName) => {
+          if (chainIdsSelected.length === 0) return true;
+
+          return chainIdsSelected.some((chainIdSelected) =>
+            Object.keys(explorersData[explorerName].chainIdToLabel).includes(
+              chainIdSelected.toString()
+            )
+          );
+        })
     );
-  }, [searchExplorer, explorersData]);
+  }, [searchExplorer, chainIdsSelected, explorersData]);
 
   return (
     <Box>
@@ -64,8 +101,81 @@ export const ExplorerGridBase = ({
             </InputRightElement>
           )}
         </InputGroup>
+        <Popover>
+          <PopoverTrigger>
+            <Button ml="0.5rem" size={"xs"} py={"1.2rem"}>
+              <HStack>
+                <FontAwesomeIcon icon={faFilter} size="lg" />
+                <Text>
+                  Chains{" "}
+                  {chainIdsSelected.length > 0 &&
+                    `(${chainIdsSelected.length})`}
+                </Text>
+              </HStack>
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent w="11rem">
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <PopoverBody bg="bg.900">
+              <Text>Select chains:</Text>
+              <SimpleGrid>
+                {chainIdOptions.map((chainId, i) => (
+                  <GridItem key={i}>
+                    <Button
+                      pr="1.5rem"
+                      size="xs"
+                      variant={"ghost"}
+                      onClick={() => {
+                        // remove from the array
+                        if (chainIdsSelected.includes(chainId)) {
+                          setChainIdsSelected(
+                            chainIdsSelected.filter((id) => id !== chainId)
+                          );
+                        } else {
+                          setChainIdsSelected([...chainIdsSelected, chainId]);
+                        }
+                      }}
+                      bg={
+                        chainIdsSelected.includes(chainId)
+                          ? "whiteAlpha.300"
+                          : ""
+                      }
+                    >
+                      <HStack>
+                        <Image
+                          src={chainIdToImage[chainId]}
+                          alt={chainIdToChain[chainId].name}
+                          w="1rem"
+                          bg="white"
+                          rounded="full"
+                        />
+                        <Text>{chainIdToChain[chainId].name}</Text>
+                      </HStack>
+                    </Button>
+                  </GridItem>
+                ))}
+                {chainIdsSelected.length > 0 && (
+                  <GridItem>
+                    <Center>
+                      <Button
+                        size="xs"
+                        variant={"ghost"}
+                        onClick={() => setChainIdsSelected([])}
+                        bg={"whiteAlpha.300"}
+                      >
+                        <Text>Clear</Text>
+                      </Button>
+                    </Center>
+                  </GridItem>
+                )}
+              </SimpleGrid>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
       </Center>
       <Box
+        mt="1rem"
         px="1rem"
         minH="30rem"
         maxH="30rem"
