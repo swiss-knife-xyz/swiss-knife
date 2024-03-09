afe2e4666c6e26a873cb1bf86513a19059ec8f36
apoorvlathey <latheyapoorv@gmail.com>
2024-03-10 05:18:29 +0530

    revert: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,9 +22,6 @@ export const Footer = () => {
       borderTop={"solid"}
       borderTopWidth={1}
       borderColor={"custom.greenDarker"}
-      position="absolute"
-      bottom={0}
-      width="100%"
     >
       <Container as={Stack} maxW={"8xl"} py={10}>
         <VStack spacing={5}>
