481e165dc7a705bf6fcc757bf16cd3e042cd0057
apoorvlathey <latheyapoorv@gmail.com>
2024-04-10 18:36:40 +0530

    feat: decoder AddressParam: getEns name and avatar for addresses

--- components/fnParams/AddressParam.tsx
+++ components/fnParams/AddressParam.tsx
@@ -1,14 +1,112 @@
-import React from "react";
-import { InputField } from "../InputField";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  HStack,
+  Link,
+  Box,
+  Center,
+  Text,
+  Avatar,
+  InputGroup,
+  Input,
+  InputRightElement,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Hex } from "viem";
+import { normalize } from "viem/ens";
+import { getPath, publicClient } from "@/utils";
+import subdomains from "@/subdomains";
+import { CopyToClipboard } from "@/components/CopyToClipboard";
 
 interface Params {
   value: string;
 }
 
-// TODO: add ens + avatar
-// TODO: add link to swiss-knife explorer (add ExternalIcon on the right)
-export const AddressParam = ({ value }: Params) => {
+export const AddressParam = ({ value: address }: Params) => {
+  const [ensName, setEnsName] = useState("");
+  const [ensAvatar, setEnsAvatar] = useState("");
+  const [showEns, setShowEns] = useState(false);
+
+  const [value, setValue] = useState<string>(address);
+
+  const getEnsName = async () => {
+    return await publicClient.getEnsName({
+      address: address as Hex,
+    });
+  };
+
+  const getEnsAvatar = async () => {
+    return await publicClient.getEnsAvatar({
+      name: normalize(ensName),
+    });
+  };
+
+  useEffect(() => {
+    getEnsName().then((res) => {
+      if (res) {
+        setEnsName(res);
+        setShowEns(true);
+      }
+    });
+  }, [address]);
+
+  useEffect(() => {
+    if (ensName) {
+      getEnsAvatar().then((res) => {
+        console.log({
+          ensName,
+          avatar: res,
+        });
+        if (res) {
+          setEnsAvatar(res);
+        }
+      });
+    }
+  }, [ensName]);
+
+  useEffect(() => {
+    setValue(showEns ? ensName : address);
+  }, [showEns, ensName, address]);
+
   return (
-    <InputField value={value} placeholder="" isReadOnly onChange={() => {}} />
+    <Box>
+      <HStack>
+        {ensName ? (
+          <Button
+            onClick={() => {
+              setShowEns(!showEns);
+            }}
+            size={"xs"}
+            px={4}
+            py={5}
+          >
+            {showEns ? "Address" : "ENS"}
+          </Button>
+        ) : null}
+        <InputGroup>
+          {ensAvatar ? (
+            <InputLeftElement>
+              <Avatar src={ensAvatar} w={"1.2rem"} h={"1.2rem"} />
+            </InputLeftElement>
+          ) : null}
+          <Input value={value} isReadOnly />
+          <InputRightElement pr={1}>
+            <CopyToClipboard textToCopy={value ?? ""} />
+          </InputRightElement>
+        </InputGroup>
+        <Button size={"sm"}>
+          <Link
+            href={`${getPath(subdomains.EXPLORER)}address/${value}`}
+            title="View on explorer"
+            isExternal
+          >
+            <HStack>
+              <ExternalLinkIcon />
+            </HStack>
+          </Link>
+        </Button>
+      </HStack>
+    </Box>
   );
 };
