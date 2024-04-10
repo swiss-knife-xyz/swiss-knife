2aaeb0a62dd2e39c70b29c91665dbb24db4d0c74
apoorvlathey <latheyapoorv@gmail.com>
2024-04-11 03:54:41 +0530

    chore: add metadata to index

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,14 @@
-import { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import { Providers } from "./providers";
-import { Analytics } from "@/components/Analytics";
+import { IndexLayout as IndexLayoutC } from "@/components/layouts/IndexLayout";
 import { getMetadata } from "@/utils";
 
-const poppins = Poppins({ weight: "400", subsets: ["latin"] });
+export const metadata = getMetadata({
+  title: "Swiss-Knife.xyz | All your Ethereum dev tools at one place!",
+  description: "All your Ethereum dev tools at one place!",
+  images: "https://swiss-knife.xyz/og/index.png",
+});
 
-// export const metadata = getMetadata({
-//   title: "Swiss Knife",
-//   description: "All your Ethereum dev tools at one place!",
-//   images: "https://swiss-knife.xyz/og/index.png",
-// });
-
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <html lang="en">
-      <body className={poppins.className}>
-        <Analytics />
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+const IndexLayout = ({ children }: { children: React.ReactNode }) => {
+  return <IndexLayoutC>{children}</IndexLayoutC>;
 };
 
-export default RootLayout;
+export default IndexLayout;
---
+++ components/layouts/IndexLayout.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { Providers } from "@/app/providers";
+import { Analytics } from "@/components/Analytics";
+import { getMetadata } from "@/utils";
+
+const poppins = Poppins({ weight: "400", subsets: ["latin"] });
+
+export const IndexLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <Analytics />
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+};
