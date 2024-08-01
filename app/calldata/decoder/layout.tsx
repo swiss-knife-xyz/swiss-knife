b1c0b9f744c3690c557f855a2d4d0062d11b72c4
apoorvlathey <latheyapoorv@gmail.com>
2024-08-01 07:24:01 +0530

    chore: improve SEO metadata calldata decoder

--- app/calldata/decoder/layout.tsx
+++ app/calldata/decoder/layout.tsx
@@ -1,9 +1,9 @@
 import { getMetadata } from "@/utils";
 
 export const metadata = getMetadata({
-  title: "Universal Calldata Decoder | Swiss-Knife.xyz",
+  title: "ETH Calldata Decoder | Swiss-Knife.xyz",
   description:
-    "Decode any calldata, and view the parameters in a human-readable format, even without having the contract ABI.",
+    "Decode any calldata, and view the parameters in a human-readable format, even without having the contract ABI with this Universal ETH Calldata Decoder.",
   images: "https://swiss-knife.xyz/og/calldata-decoder.png",
 });
 
