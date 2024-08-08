95023fae947faf73e7ebdffc91a04d423728821b
Anupriya Lathey <90963726+anupriyalathey@users.noreply.github.com>
2024-08-08 05:51:25 +0530

    feat: converter: eth: add 10^x converter field (#29)
    * chore: converter: eth: add 10^6 converter field
    
    * chore: converter: eth: add custom converter field
    
    ---------
    
    Co-authored-by: anupriya <anupriyalathey.com>

--- app/converter/eth/page.tsx
+++ app/converter/eth/page.tsx
@@ -1,8 +1,23 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Heading, Table, Tbody, Tr, Td, Text } from "@chakra-ui/react";
-import { parseEther, parseGwei, formatEther, formatGwei } from "viem";
+import {
+  Heading,
+  Table,
+  Tbody,
+  Tr,
+  Td,
+  Text,
+  Input,
+  Box,
+} from "@chakra-ui/react";
+import {
+  parseEther,
+  parseGwei,
+  formatEther,
+  formatGwei,
+  formatUnits,
+} from "viem";
 import { InputField } from "@/components/InputField";
 import { Label } from "@/components/Label";
 import { useLocalStorage } from "usehooks-ts";
@@ -11,13 +26,15 @@ const ETHUnitConverter = () => {
   const [wei, setWei] = useState<string>();
   const [gwei, setGwei] = useState<string>();
   const [eth, setEth] = useState<string>();
+  const [unit, setUnit] = useState<string>();
   const [usd, setUsd] = useState<string>();
+  const [exponent, setExponent] = useState<number>(6);
 
   const [ethPrice, setEthPrice] = useLocalStorage("ethPrice", 0);
 
   const handleOnChange = (
     e: React.ChangeEvent<HTMLInputElement>,
-    unit: "wei" | "gwei" | "eth" | "usd",
+    unit: "wei" | "gwei" | "eth" | "unit" | "usd",
     valueToWei: (value: string) => string
   ) => {
     const value = e.target.value;
@@ -28,6 +45,7 @@ const ETHUnitConverter = () => {
     if (unit === "wei") setWei(value);
     else if (unit === "gwei") setGwei(value);
     else if (unit === "eth") setEth(value);
+    else if (unit === "unit") setUnit(value);
     else if (unit === "usd") setUsd(value);
 
     if (value.length > 0) {
@@ -37,19 +55,24 @@ const ETHUnitConverter = () => {
       setWei("");
       setGwei("");
       setEth("");
+      setUnit("");
       setUsd("");
     }
   };
 
   const setValues = (
     inWei: string,
-    exceptUnit: "wei" | "gwei" | "eth" | "usd"
+    exceptUnit: "wei" | "gwei" | "eth" | "unit" | "usd"
   ) => {
     setWei(inWei);
 
     if (inWei.length > 0) {
       if (exceptUnit !== "gwei") setGwei(formatGwei(BigInt(inWei)));
       if (exceptUnit !== "eth") setEth(formatEther(BigInt(inWei)));
+      if (exceptUnit !== "unit") {
+        const unitValue = formatUnits(BigInt(inWei), 18 - exponent);
+        setUnit(unitValue);
+      }
       if (exceptUnit !== "usd") {
         const eth = formatEther(BigInt(inWei));
         setUsd((parseFloat(eth) * ethPrice).toString());
@@ -57,6 +80,7 @@ const ETHUnitConverter = () => {
     } else {
       setGwei("");
       setEth("");
+      setUnit("");
       setUsd("");
     }
   };
@@ -73,9 +97,20 @@ const ETHUnitConverter = () => {
     } catch (error) {}
   };
 
+  const recalculateUnit = () => {
+    if (wei) {
+      const unitValue = formatUnits(BigInt(wei), 18 - exponent);
+      setUnit(unitValue);
+    }
+  };
+
   useEffect(() => {
     setPrices();
   }, []);
+
+  useEffect(() => {
+    recalculateUnit();
+  }, [exponent]);
 
   return (
     <>
@@ -83,7 +118,10 @@ const ETHUnitConverter = () => {
       <Table mt={"3rem"} variant={"unstyled"}>
         <Tbody>
           <Tr>
-            <Label>Wei</Label>
+            <Label>
+              <Text>Wei</Text>
+              <Text opacity={0.6}>(10^18)</Text>
+            </Label>
             <Td>
               <InputField
                 autoFocus
@@ -95,7 +133,10 @@ const ETHUnitConverter = () => {
             </Td>
           </Tr>
           <Tr>
-            <Label>Gwei</Label>
+            <Label>
+              <Text>Gwei</Text>
+              <Text opacity={0.6}>(10^9)</Text>
+            </Label>
             <Td>
               <InputField
                 type="number"
@@ -110,7 +151,44 @@ const ETHUnitConverter = () => {
             </Td>
           </Tr>
           <Tr>
-            <Label>Ether</Label>
+            <Label>
+              <Box display="inline-flex" alignItems="center">
+                <Text>10^</Text>
+                <Input
+                  type="number"
+                  placeholder="Enter custom exponent"
+                  value={exponent.toString()}
+                  onChange={(e) => {
+                    const newExponent = Number(e.target.value);
+                    if (!isNaN(newExponent) && newExponent >= 0) {
+                      setExponent(newExponent);
+                    }
+                  }}
+                  width="55px"
+                />
+              </Box>
+            </Label>
+            <Td>
+              <InputField
+                type="number"
+                placeholder={`10^${exponent}`}
+                value={unit}
+                onChange={(e) =>
+                  handleOnChange(e, "unit", (value) =>
+                    parseEther(
+                      (parseFloat(value) / 10 ** exponent).toString()
+                    ).toString()
+                  )
+                }
+              />
+            </Td>
+          </Tr>
+
+          <Tr>
+            <Label>
+              <Text>Ether</Text>
+              <Text opacity={0.6}>(1)</Text>
+            </Label>
             <Td>
               <InputField
                 type="number"
