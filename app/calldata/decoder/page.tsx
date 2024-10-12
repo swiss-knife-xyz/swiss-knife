d5e89f16828ba59519d9cab431675e3ce80e6118
apoorvlathey <latheyapoorv@gmail.com>
2024-10-12 19:25:16 +0400

    decoder: show function name in title on link share, with tx inputs

--- app/calldata/decoder/page.tsx
+++ app/calldata/decoder/page.tsx
@@ -3,6 +3,8 @@ import { getMetadata, startHexWith0x } from "@/utils";
 import type { Metadata } from "next";
 // Putting the page into separate component as it uses "use client" which doesn't work with `generateMetadata`
 import { CalldataDecoderPage as CalldataDecoderP } from "@/components/pages/CalldataDecoderPage";
+import { Chain, createPublicClient, Hex, http } from "viem";
+import { c, chainIdToChain } from "@/data/common";
 
 interface PageProps {
   searchParams: { [key: string]: string | string[] | undefined };
@@ -14,8 +16,66 @@ export async function generateMetadata({
   let title = "ETH Calldata Decoder | Swiss-Knife.xyz";
 
   let calldata = searchParams.calldata as string | undefined;
+  let tx = searchParams.tx as string | undefined;
 
   // add function name to the title if possible
+  if (tx) {
+    let chainId = searchParams.chainId as string | undefined;
+    let chain: Chain;
+
+    try {
+      let txHash: string;
+      if (/^0x([A-Fa-f0-9]{64})$/.test(tx)) {
+        txHash = tx;
+
+        if (!chainId) throw new Error("Chain ID not provided");
+        chain = chainIdToChain[parseInt(chainId)];
+      } else {
+        txHash = tx.split("/").pop()!;
+
+        const chainKey = Object.keys(c).filter((chainKey) => {
+          const chain = c[chainKey as keyof typeof c] as Chain;
+
+          // using "null" instead of "" because tx.split("/") contains ""
+          let explorerDomainDefault = "null";
+          let explorerDomainEtherscan = "null";
+          if (chain.blockExplorers) {
+            explorerDomainDefault = chain.blockExplorers.default.url
+              .split("//")
+              .pop()!;
+
+            if (chain.blockExplorers.etherscan) {
+              explorerDomainEtherscan = chain.blockExplorers.etherscan.url
+                .split("//")
+                .pop()!;
+            }
+          }
+
+          return (
+            tx
+              .split("/")
+              .filter(
+                (urlPart) =>
+                  urlPart.toLowerCase() ===
+                    explorerDomainDefault.toLowerCase() ||
+                  urlPart.toLowerCase() ===
+                    explorerDomainEtherscan.toLowerCase()
+              ).length > 0
+          );
+        })[0];
+        chain = c[chainKey as keyof typeof c];
+      }
+
+      const publicClient = createPublicClient({
+        chain,
+        transport: http(),
+      });
+      const transaction = await publicClient.getTransaction({
+        hash: txHash as Hex,
+      });
+      calldata = transaction.input;
+    } catch {}
+  }
   if (calldata) {
     calldata = startHexWith0x(calldata);
     const selector = calldata.slice(0, 10);
