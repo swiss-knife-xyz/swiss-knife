8bd68465530c58b2f46e06e1c841f879b365f9ae
apoorvlathey <latheyapoorv@gmail.com>
2024-10-12 19:08:51 +0400

    decoder: show function name in the page title in og tags when sharing links

--- utils/index.ts
+++ utils/index.ts
@@ -113,42 +113,6 @@ export const ethFormatOptions = [
   "Days",
 ];
 
-export const fetchFunctionInterface = async (
-  selector: string
-): Promise<any[]> => {
-  // from api.openchain.xyz
-  const response = await axios.get(
-    "https://api.openchain.xyz/signature-database/v1/lookup",
-    {
-      params: {
-        function: selector,
-      },
-    }
-  );
-  const results = response.data.result.function[selector].map(
-    (f: { name: string }) => f.name
-  );
-
-  if (results.length > 0) {
-    return results;
-  } else {
-    // from 4byte.directory
-    const response = await axios.get(
-      "https://www.4byte.directory/api/v1/signatures/",
-      {
-        params: {
-          hex_signature: selector,
-        },
-      }
-    );
-    const results = response.data.results.map(
-      (f: { text_signature: string }) => f.text_signature
-    );
-
-    return results;
-  }
-};
-
 export const slicedText = (txt: string) => {
   return txt.length > 10
     ? `${txt.slice(0, 6)}...${txt.slice(txt.length - 4, txt.length)}`
