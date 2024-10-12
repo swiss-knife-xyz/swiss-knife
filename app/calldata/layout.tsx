28ad55a117d7d2180706925c3fad135b3e7d27fe
apoorvlathey <latheyapoorv@gmail.com>
2024-10-12 16:15:26 +0400

    fix page title to “ETH Calldata Decoder” for SEO

--- app/calldata/layout.tsx
+++ app/calldata/layout.tsx
@@ -2,7 +2,7 @@ import { CalldataLayout as CalldataLayoutC } from "@/components/layouts/Calldata
 import { getMetadata } from "@/utils";
 
 export const metadata = getMetadata({
-  title: "Calldata Decoder | Swiss-Knife.xyz",
+  title: "ETH Calldata Decoder | Swiss-Knife.xyz",
   description:
     "Decode any calldata, and view the parameters in a human-readable format, even without having the contract ABI.",
   images: "https://swiss-knife.xyz/og/calldata-decoder.png",
