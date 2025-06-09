006898fca3064af08d0a984757318b816d4d10c2
apoorvlathey <latheyapoorv@gmail.com>
2025-06-09 17:03:44 +0400

    Signatures: update metadata

--- app/wallet/signatures/layout.tsx
+++ app/wallet/signatures/layout.tsx
@@ -2,7 +2,7 @@ import { getMetadata } from "@/utils";
 
 export const metadata = getMetadata({
   title: "Signatures | Swiss-Knife.xyz",
-  description: "View signatures",
+  description: "Sign and Verify any message or 712 Typed Data",
   images: "https://swiss-knife.xyz/og/wallet-signatures.png",
 });
 
