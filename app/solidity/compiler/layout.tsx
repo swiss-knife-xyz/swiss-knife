4ff2ff26877e37b9e48530739524580e211b3c8e
apoorvlathey <latheyapoorv@gmail.com>
2025-10-30 08:47:45 +0530

    Solidity Compiler: add og image

--- app/solidity/compiler/layout.tsx
+++ app/solidity/compiler/layout.tsx
@@ -2,8 +2,8 @@ import { getMetadata } from "@/utils";
 
 export const metadata = getMetadata({
   title: "Solidity Compiler | Swiss-Knife.xyz",
-  description: "Compile solidity contracts with the solc compiler.",
-  images: "https://swiss-knife.xyz/og/",
+  description: "Compile solidity contracts & quickly generate ABI.",
+  images: "https://swiss-knife.xyz/og/solidity-compiler.png",
 });
 
 const CompilerLayout = ({ children }: { children: React.ReactNode }) => {
