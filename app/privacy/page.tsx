3b3212feae33a0f62093ce05d9f01cf8dc304066
apoorvlathey <apoorv@eth.sh>
2026-07-09 11:42:15 +0400

    fix: use literal privacy revalidate

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,11 +1,8 @@
 import PrivacyPageClient from "./PrivacyPageClient";
-import {
-  getCachedLinkPreview,
-  LINK_PREVIEW_REVALIDATE_SECONDS,
-} from "@/lib/linkPreview";
+import { getCachedLinkPreview } from "@/lib/linkPreview";
 import { externalDashboardLinks } from "./data";
 
-export const revalidate = LINK_PREVIEW_REVALIDATE_SECONDS;
+export const revalidate = 604800;
 
 const PrivacyPage = async () => {
   const externalDashboards = await Promise.all(
