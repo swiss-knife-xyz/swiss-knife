ec606e44d424899015ef57989a8b37300cba9dc1
apoorvlathey <apoorv@eth.sh>
2026-07-14 16:54:22 +0400

    feat: add BANKR LayerZero bridge

--- data/chains.ts
+++ data/chains.ts
@@ -26,12 +26,13 @@ import {
   zora,
   Chain,
 } from "wagmi/chains";
-import { plasma, megaeth, citrea, monad } from "./common";
+import { plasma, megaeth, citrea, monad, robinhood } from "./common";
 
 export const walletChains: readonly [Chain, ...Chain[]] = [
   // first chain is the default
   mainnet,
   base,
+  robinhood,
   arbitrum,
   avalanche,
   bsc,
